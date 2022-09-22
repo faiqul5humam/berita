1433ec2fe2388f2b1b9d9c7188d49fbe812ec363
faiqul5humam <humamku45@gmail.com>
2022-09-22 19:58:34 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Detik = require('./web/detik.js');
+const Antara = require('./web/antara.js');
+const Kompas = require('./web/kompas.js');
+
+module.exports = {
+    Detik: Detik,
+    Antara: Antara,
+    Kompas: Kompas,
+}
